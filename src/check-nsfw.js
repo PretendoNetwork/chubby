29e4bb9b6b9a24e66865fcd678957c88f1e57315
Monty <monty.github@gmail.com>
2021-05-29 22:59:23 +0200

    Various fixes following feedback

--- src/check-nsfw.js
+++ src/check-nsfw.js
@@ -68,8 +68,8 @@ async function checkNSFW(message, urls) {
 
 				// aggressive NSFW check
 				// flag the GIF if ANY NSFW frame is found
+				if (frameClassification.probability < 0.8) continue;  // if low confidence then do nothing
 				if (frameClassification.className === 'Porn' || frameClassification.className === 'Hentai' || frameClassification.className === 'Sexy') {
-					if (frameClassification.probability < 0.8) return;  // if low confidence then do nothing
 					predictions = framePredictions;
 					suspectedUrls.push(url); // if suspected as NSFW then track it
 					break; // end the loop if ANY NSFW frame is found
@@ -86,9 +86,9 @@ async function checkNSFW(message, urls) {
 				return (previous.probability > current.probability) ? previous : current;
 			});
 
+			if (classification.probability < 0.8) return;  // if low confidence then do nothing
 			// check if the prediction is black listed
 			if (classification.className === 'Porn' || classification.className === 'Hentai' || classification.className === 'Sexy') {
-				if (classification.probability < 0.8) return; // if low confidence then do nothing
 				suspectedUrls.push(url); // if suspected as NSFW then track it
 			}
 		}
