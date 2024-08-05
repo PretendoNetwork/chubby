acf6d216d971c3462031c57d2d3d9c09cbd4abef
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2024-08-04 23:16:33 -0400

    feat: implement soft-bans in /kick

--- src/commands/kick.ts
+++ src/commands/kick.ts
@@ -16,6 +16,7 @@ async function kickHandler(interaction: ChatInputCommandInteraction): Promise<vo
 	const executor = interaction.user;
 	const users = interaction.options.getString('users', true);
 	const reason = interaction.options.getString('reason', true);
+	const deleteMessages = interaction.options.getNumber('delete_messages');
 
 	const userIds = [...new Set(Array.from(users.matchAll(new RegExp(MessageMentions.UsersPattern, 'g')), match => match[1]))];
 
@@ -53,6 +54,10 @@ async function kickHandler(interaction: ChatInputCommandInteraction): Promise<vo
 			{
 				name: 'Reason',
 				value: reason
+			},
+			{
+				name: 'Delete Messages (seconds)',
+				value: deleteMessages?.toString() ?? 'No'
 			},
 			{
 				name: 'From bot /kick command',
@@ -109,7 +114,11 @@ async function kickHandler(interaction: ChatInputCommandInteraction): Promise<vo
 			sendMemberEmbeds.push(banEmbed);
 		} else { // Just kick
 			eventLogEmbed.setColor(0xEF7F31);
-			eventLogEmbed.setTitle('Event Type: _Member Kicked_');
+			if (deleteMessages) {
+				eventLogEmbed.setTitle('Event Type: _Member Soft-Banned_');
+			} else {
+				eventLogEmbed.setTitle('Event Type: _Member Kicked_');
+			}
 
 			const kickEmbed = new EmbedBuilder();
 
@@ -178,10 +187,19 @@ async function kickHandler(interaction: ChatInputCommandInteraction): Promise<vo
 		});
 
 		if (isKick) {
-			await member.kick(reason);
+			if (deleteMessages) {
+				await member.ban({
+					reason: `Soft-ban: ${reason}`,
+					deleteMessageSeconds: deleteMessages ?? undefined
+				});
+				await guild.members.unban(member, 'Soft-ban removed');
+			} else {
+				await member.kick(reason);
+			}
 		} else if (isBan) {
 			await member.ban({
-				reason
+				reason,
+				deleteMessageSeconds: deleteMessages ?? undefined
 			});
 
 			await Ban.create({
@@ -221,6 +239,20 @@ const command = new SlashCommandBuilder()
 		return option.setName('reason')
 			.setDescription('Reason for the kick')
 			.setRequired(true);
+	})
+	.addNumberOption(option => {
+		return option.setName('delete_messages')
+			.setDescription('How much of their recent message history to delete (turns this kick into a soft-ban)')
+			.addChoices(
+				{ name: 'Previous 30 Minutes', value: 30 * 60 },
+				{ name: 'Previous Hour', value: 60 * 60 },
+				{ name: 'Previous 3 Hours', value: 3 * 60 * 60 },
+				{ name: 'Previous 6 Hours', value: 6 * 60 * 60 },
+				{ name: 'Previous 12 Hours', value: 12 * 60 * 60 },
+				{ name: 'Previous Day', value: 24 * 60 * 60 },
+				{ name: 'Previous 3 Days', value: 3 * 24 * 60 * 60 },
+				{ name: 'Previous Week', value: 7 * 24 * 60 * 60 },
+			);
 	});
 
 export default {
