63d3bb5b8e70bdc1b552106dfa63007deccee3b9
mrjvs <jellevs@gmail.com>
2025-08-01 22:05:21 +0200

    Disable sequelize logging

--- src/sequelize-instance.ts
+++ src/sequelize-instance.ts
@@ -2,5 +2,6 @@ import { Sequelize } from 'sequelize';
 import config from '@/config';
 
 export const sequelize = new Sequelize(config.sequelize.postgres_uri, {
-	dialect: 'postgres'
+	dialect: 'postgres',
+	logging: false
 });
